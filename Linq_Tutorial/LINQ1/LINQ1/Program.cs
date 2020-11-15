8874f0b6863a97310c44d5199e6e05a4a2f9326a
WebsterSweden <webster@webster.se>
2020-11-15 16:29:58 +0100

    Edit Linq_Tutorial.

--- Linq_Tutorial/LINQ1/LINQ1/Program.cs
+++ Linq_Tutorial/LINQ1/LINQ1/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ1
 {
@@ -6,6 +9,64 @@ namespace LINQ1
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Windows";
+
+            ShowLargeFilesWithoutLinq(path);
+
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            //var query = from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file;
+
+            //foreach (var file in query.Take(5))
+            //{
+            //    Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            //}
+
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(5);
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            Console.WriteLine(directory);
+
+            FileInfo[] files = directory.GetFiles();
+
+            Array.Sort(files, new FileInfoComparer());
+
+            //foreach (FileInfo file in files)
+            //{
+            //    Console.WriteLine($"{file.Name} : {file.Length}");
+            //}
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
+
+// dir /os
